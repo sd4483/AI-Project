16d784b086fd9b0e4f1ebf88b6833cf030f6e323
sd4483 <sudheer.paturi@hotmail.com>
2018-06-16 18:44:39 +0800

    Symptom 4

--- crawler.py
+++ crawler.py
@@ -26,5 +26,10 @@ X.Symptom7 = X.Symptom7.fillna('Not defined')
 
 encode_gender=pd.get_dummies(X['Gender'], prefix='Gender')
 encode_nationality = pd.get_dummies(X['Nationality'], prefix = 'Nationality')
-encode_symptom1
-encode_synptom2
+encode_symptom1 = pd.get_dummies(X['Symptom1'], prefix = 'Symptom1')
+encode_synptom2 = pd.get_dummies(X['Symptom2'], prefix = 'Symptom2')
+encode_symptom3 = pd.get_dummies(X['Symptom3'], prefix = 'Symptom3')
+encode_symptom4 = pd.get_dummies(X['Symptom4'], prefix = 'Symptom4')
+encode_symptom5
+encode_symptom6
+encode_symptom7
